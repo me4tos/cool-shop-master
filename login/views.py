75a937f345a419b6f66e759fcbf8df46d7a0d7c9
root <thomascibersport@gmail.com>
2024-09-24 10:42:00 +0300

    init

---
+++ login/views.py
@@ -0,0 +1,42 @@
+from django.db.transaction import commit
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login
+from django.http import HttpRequest, HttpResponse
+from . import forms
+
+
+def login_view(request: HttpRequest) -> HttpResponse:
+    if request.method == 'POST':
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(username = username, password = password)
+
+        if user is None:
+            return redirect('/login')
+
+        else: 
+            login(request, user)
+            return redirect('/')
+        
+    return render(request, 'login/login.html')
+
+
+def register_view(request: HttpRequest) -> HttpResponse:
+    if request.method == "GET":
+        form = forms.RegisterForm()
+        return render(request, 'login/register.html', {
+            'form': form
+        })
+
+    if request.method == "POST":
+        form = forms.RegisterForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("/")
+
+        else:
+            return render(request, 'login/register.html', {
+                'form': form
+            })
